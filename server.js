91614765a8de500d6dff4fe7635a1306f4ad3364
Yolanda Jacobs <y.jacobs@hotmail.com>
2022-08-15 15:24:01 -0400

    file setup, created sercer.js and setup html routes

---
+++ server.js
@@ -0,0 +1,20 @@
+const express = require('express');
+
+const PORT = process.env.PORT || 3001;
+const app = express();
+const apiRoutes = require('./routes/apiRoutes');
+const htmlRoutes = require('./routes/htmlRoutes');
+
+// parse incoming string or array data
+app.use(express.urlencoded({ extended: true }));
+// parse incoming JSON data
+app.use(express.json());
+app.use(express.static('public'));
+
+// Use apiRoutes
+app.use('/api', apiRoutes);
+app.use('/', htmlRoutes);
+
+app.listen(PORT, () => {
+  console.log(`API server now on port ${PORT}!`);
+});
